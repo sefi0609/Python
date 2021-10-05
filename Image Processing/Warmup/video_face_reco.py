9855c8e06dc60fc064fb1b0de9b130abe857da74
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2021-10-05 18:03:47 +0300

    Add files via upload

---
+++ Image Processing/Warmup/video_face_reco.py
@@ -0,0 +1,23 @@
+import cv2
+#creating an video object - starting thr video from your computer camera
+video = cv2.VideoCapture(0)
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+i = 1
+#capturing frames and displaying them
+while True:
+    i += 1
+    check,frame = video.read()
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray_img,
+                                    scaleFactor = 1.1,
+                                    minNeighbors = 5)
+    for x,y,w,h in faces:
+        frame_detect = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),6)
+    cv2.imshow('video',frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+#numbers of frames
+print(i)    
+cv2.destroyAllWindows()
+video.release()
